9a007136b63ef8b0f7bb927f5744c1d4471f38b6
Maarten Nusteling <maarten.nusteling@gmail.com>
2020-12-19 21:31:48 +0100

    Added monolithic repository validation

--- src/Application/Client/Kernel.php
+++ src/Application/Client/Kernel.php
@@ -6,6 +6,7 @@ namespace Acme\Application\Client;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
